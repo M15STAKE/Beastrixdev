dff09f61fe5f1754e331bc1c7127a1beed7ebbda
M15STAKE <114368097+M15STAKE@users.noreply.github.com>
2024-08-10 09:25:59 -0500

    BeastNFT.sol

--- BeastNFT.sol
+++ BeastNFT.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
@@ -15,24 +16,35 @@ contract BeastNFT is ERC721, Ownable {
         uint256 xp;
         string[] attacks;
         string metadata;
+        string image;
         uint256 stakedAt;
     }
 
     mapping(uint256 => Beast) public beasts;
 
-    constructor() ERC721("BeastNFT", "BNFT") {}
+    constructor() ERC721("BeastNFT", "BNFT") Ownable(msg.sender) {}
 
     // Minting New Beasts
     function mintBeast(
         address player,
         string memory name,
-        string memory metadata
+        string memory metadata,
+        string memory image
     ) public onlyOwner {
         _tokenIds.increment();
         uint256 newBeastId = _tokenIds.current();
         _mint(player, newBeastId);
 
-        beasts[newBeastId] = Beast(name, 1, 0, new string[](0), metadata, 0);
+        // Assigning the newly minted Beast to the `beasts` mapping
+       beasts[newBeastId] = Beast({
+    name: name,
+    level: 1,
+    xp: 0,
+    attacks: new string[](0), // Initialize attacks with an empty array
+    metadata: metadata,
+    image: image,
+    stakedAt: 0
+     });
     }
 
     // Updating Beast Metadata
@@ -42,8 +54,8 @@ contract BeastNFT is ERC721, Ownable {
         uint256 xp,
         string[] memory attacks,
         string memory metadata
-    ) public onlyOwner {
-        require(_exists(beastId), "Beast does not exist");
+    ) public {
+        require(ownerOf(beastId) == msg.sender, "You do not own this beast");
         Beast storage beast = beasts[beastId];
         beast.level = level;
         beast.xp = xp;
@@ -83,7 +95,15 @@ contract BeastNFT is ERC721, Ownable {
 
     // Getting Beast Metadata
     function getBeastMetadata(uint256 beastId) public view returns (string memory) {
-        require(_exists(beastId), "Beast does not exist");
+        require(ownerOf(beastId) != address(0), "Beast does not exist");
         return beasts[beastId].metadata;
     }
+
+    // Getting Beast Image
+    function getBeastImage(uint256 beastId) public view returns (string memory) {
+        require(ownerOf(beastId) != address(0), "Beast does not exist");
+        return beasts[beastId].image;
+    }
 }
+
+
